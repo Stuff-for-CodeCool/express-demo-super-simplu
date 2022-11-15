25f7adf7197f307667b5a49e2815d277a1d17187
alexsincai <alex.sincai@yahoo.co.uk>
2022-11-15 17:41:53 +0200

    commit initial

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const port = 9000;
+const app = express()
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use(bodyParser.json())
+    .get("/", (request, response) => {
+        response.send("merge");
+    })
+    .get("/api/nume", (req, res) => {
+        res.send("il cheama gogu");
+    })
+    .get("/api/nume/:id", (req, res) => {
+        //  citesc de pe server
+        const raspuns = {
+            mesaj: `il cheama ${req.params.id}`,
+        };
+        res.json(raspuns);
+    })
+    .post("/api/nou", (req, res) => {
+        //  scriu o chestie noua pe server
+        const raspuns = {
+            mesaj: "am primit urmatorul 'name': " + req.body.name,
+        };
+
+        res.json(raspuns);
+    })
+    .put("/api/nou", (req, res) => {
+        //  modific o chestie deja existenta pe server
+        const raspuns = {
+            mesaj: "modific urmatorul 'name': " + req.body.name,
+        };
+
+        res.json(raspuns);
+    })
+    .delete("/api/nou", (req, res) => {
+        //  sterg o chestie deja existenta
+    })
+    .listen(port);
